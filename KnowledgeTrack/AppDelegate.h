3da1fee1a869c56c3b0b0bee0813f69f7e7c459d
Rama <rama@Ramas-iMac.local>
2013-07-17 09:37:53 +0400

    Initial Commit

---
+++ KnowledgeTrack/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  KnowledgeTrack
+//
+//  Created by Rama on 7/17/13.
+//  Copyright (c) 2013 Rama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ KnowledgeTrack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KnowledgeTrack
+//
+//  Created by Rama on 7/17/13.
+//  Copyright (c) 2013 Rama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
